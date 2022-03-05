8d5f6f79b894359c74e4388fa946ede260a0241c
Farhan <Farhan@Farhan-PC>
2022-03-05 23:00:32 +0530

    first commit

---
+++ p2/src/p2/p2.java
@@ -0,0 +1,10 @@
+package p2;
+
+public class p2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("java for web developerss data base system my sql structured query langauge");
+	}
+
+}
